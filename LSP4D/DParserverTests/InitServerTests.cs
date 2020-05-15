a42a2dbd1e99d084835d1c2d84709695a9b343e7
Alexander Bothe <info@alexanderbothe.com>
2020-05-15 14:29:08 +0200

    GenerateFoldings_HandleMetaBlocks-Test

--- LSP4D/DParserverTests/InitServerTests.cs
+++ LSP4D/DParserverTests/InitServerTests.cs
@@ -62,5 +62,41 @@ void main(string[] args) {//3
             Assert.AreEqual(6, foldingRanges[1].EndLine);
             Assert.AreEqual(5, foldingRanges[1].EndCharacter);
         }
+        
+        [Test]
+        public void GenerateFoldings_HandleMetaBlocks()
+        {
+            Assert.IsNotNull(Client.ServerCapabilities.FoldingRangeProvider);
+            
+            Client.TextDocument.DidOpen(Lsp4DUtil.DefaultMainFile, Lsp4DUtil.DLANG, @"module main;//0
+import stdio;//1
+
+static if(true) {
+//4
+void main(string[] args) {
+    writeln(`asdf`);//6
+ }
+}
+//9
+");
+
+            var foldingRanges = Client.TextDocument.FoldingRanges(Lsp4DUtil.DefaultMainFile).Result
+                .ToList();
+            foldingRanges.Sort((r1, r2) => r1.StartLine.CompareTo(r2.StartLine));
+
+            Assert.AreEqual(2, foldingRanges.Count);
+
+            Assert.AreEqual(FoldingRangeKind.Region, foldingRanges[0].Kind);
+            Assert.AreEqual(3, foldingRanges[0].StartLine);
+            Assert.AreEqual(16, foldingRanges[0].StartCharacter);
+            Assert.AreEqual(8, foldingRanges[0].EndLine);
+            Assert.AreEqual(1, foldingRanges[0].EndCharacter);
+
+            Assert.AreEqual(FoldingRangeKind.Region, foldingRanges[1].Kind);
+            Assert.AreEqual(5, foldingRanges[1].StartLine);
+            Assert.AreEqual(25, foldingRanges[1].StartCharacter);
+            Assert.AreEqual(7, foldingRanges[1].EndLine);
+            Assert.AreEqual(2, foldingRanges[1].EndCharacter);
+        }
     }
 }
