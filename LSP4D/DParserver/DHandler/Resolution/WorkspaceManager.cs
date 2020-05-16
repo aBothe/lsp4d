794ef06c47dd379d950e61e643e72fd9d0d782f7
Alexander Bothe <info@alexanderbothe.com>
2020-05-16 17:16:37 +0200

    DLanguageServerFactory.cs: Moved workspace parse&wait code to WorkspaceManager.cs

--- LSP4D/DParserver/DHandler/Resolution/WorkspaceManager.cs
+++ LSP4D/DParserver/DHandler/Resolution/WorkspaceManager.cs
@@ -1,11 +1,58 @@
 using System.Collections.Generic;
 using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using D_Parser.Misc;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 
 namespace D_Parserver.DHandler.Resolution
 {
     public class WorkspaceManager
     {
-        public static readonly BehaviorSubject<List<string>> WorkspaceFolders 
+        public static readonly BehaviorSubject<List<string>> WorkspaceFolders
             = new BehaviorSubject<List<string>>(new List<string>());
+
+        public static async Task SetWorkspaceRoots(IEnumerable<string> workspaceFolders,
+            ProgressManager progressManager, IWorkDoneProgressParams workDoneProgressParams)
+        {
+            var completionSource = new TaskCompletionSource<bool>();
+            var initialParseProgress = progressManager.WorkDone(workDoneProgressParams, new WorkDoneProgressBegin()
+            {
+                Title = "Begin parsing workspace",
+                Percentage = 0
+            });
+
+            var partProgresses = GlobalParseCache.BeginAddOrUpdatePaths(workspaceFolders, finishedHandler:
+                ea =>
+                {
+                    initialParseProgress.OnNext(new WorkDoneProgressReport()
+                    {
+                        Message = "Done parsing workspace",
+                        Percentage = 100
+                    });
+                    initialParseProgress.OnCompleted();
+                    completionSource.SetResult(true);
+                });
+
+            int partIndex = 1;
+            foreach (var statisticsHandle in partProgresses)
+            {
+                initialParseProgress.OnNext(new WorkDoneProgressReport()
+                {
+                    Percentage = partIndex / partProgresses.Count,
+                    Message = "Parse " + statisticsHandle.basePath
+                });
+                statisticsHandle.WaitForCompletion();
+                partIndex++;
+
+                initialParseProgress.OnNext(new WorkDoneProgressReport()
+                {
+                    Percentage = partIndex / partProgresses.Count,
+                    Message = "Finished parsing " + statisticsHandle.basePath
+                });
+            }
+
+            await completionSource.Task;
+        }
     }
 }
--- LSP4D/DParserver/DLanguageServerFactory.cs
+++ LSP4D/DParserver/DLanguageServerFactory.cs
@@ -2,9 +2,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using D_Parser.Misc;
 using D_Parserver.DHandler;
-using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using D_Parserver.DHandler.Resolution;
 using OmniSharp.Extensions.LanguageServer.Server;
 
 namespace D_Parserver
@@ -20,48 +19,16 @@ namespace D_Parserver
                     .WithHandler<FoldingRangeHandler>()
                     .WithHandler<DDefinitionHandler>()
                     .WithHandler<TextDocumentHandler>()
-                    .OnInitialize(async (server, request) => {
-                        var completionSource = new TaskCompletionSource<bool>();
-                        var initialParseProgress = server.ProgressManager.WorkDone(request, new WorkDoneProgressBegin() {
-                            Title = "Begin parsing workspace",
-                            Percentage = 0
-                        });
-                        
+                    .OnInitialize(async (server, request) =>
+                    {
                         var workspaceFolders = new HashSet<string>();
                         workspaceFolders.Add(request.RootPath);
                         if (request.WorkspaceFolders != null)
                         {
                             request.WorkspaceFolders.Select(folder => workspaceFolders.Add(folder.Uri.AbsolutePath));
                         }
-                        
-                        var partProgresses = GlobalParseCache.BeginAddOrUpdatePaths(workspaceFolders, finishedHandler:
-                            ea =>
-                            {
-                                initialParseProgress.OnNext(new WorkDoneProgressReport() {
-                                    Message = "Done parsing workspace",
-                                    Percentage = 100
-                                });
-                                initialParseProgress.OnCompleted();
-                                completionSource.SetResult(true);
-                            });
 
-                        int partIndex = 1;
-                        foreach (var statisticsHandle in partProgresses)
-                        {
-                            initialParseProgress.OnNext(new WorkDoneProgressReport() {
-                                Percentage = partIndex / partProgresses.Count,
-                                Message = "Parse " + statisticsHandle.basePath
-                            });
-                            statisticsHandle.WaitForCompletion();
-                            partIndex++;
-                            
-                            initialParseProgress.OnNext(new WorkDoneProgressReport() {
-                                Percentage = partIndex / partProgresses.Count,
-                                Message = "Finished parsing " + statisticsHandle.basePath
-                            });
-                        }
-
-                        await completionSource.Task;
+                        await WorkspaceManager.SetWorkspaceRoots(workspaceFolders, server.ProgressManager, request);
                     })
             );
         }
