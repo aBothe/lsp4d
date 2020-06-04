6b48beed242e9708ba1acaa6f91611899ea0c31b
Alexander Bothe <info@alexanderbothe.com>
2020-06-04 21:20:01 +0200

    DDefinitionHandler: Test going to a constructor (instead to the class declaration itself) when having selecting a NewExpression

--- LSP4D/DParserverTests/DDefinitionHandlerTests.cs
+++ LSP4D/DParserverTests/DDefinitionHandlerTests.cs
@@ -33,5 +33,26 @@ My§Class instance;");
                             "\"TargetSelectionRange\":{\"Start\":{\"Line\":1,\"Character\":6},\"End\":{\"Line\":1,\"Character\":13}}" +
                             "}}]", JsonConvert.SerializeObject(definitions));
         }
+
+        [Test]
+        public void CallGotoDeclarationHandler_OnNewExpression_ReturnsConstructorMethodLocations()
+        {
+            var caret = OpenMainFile(@"module main;
+class MyClass(T) {
+    this() {}
+    this(T a) {}
+}
+auto b = new MyCla§ss!int();");
+
+            var definitions = Client.TextDocument
+                .Definition(Lsp4DUtil.DefaultMainFile, (int) caret.Line, (int) caret.Character).Result.ToList();
+
+            Assert.AreEqual("[{\"IsLocation\":false,\"Location\":null,\"IsLocationLink\":true,\"LocationLink\":{" +
+                            "\"OriginSelectionRange\":{\"Start\":{\"Line\":5,\"Character\":9},\"End\":{\"Line\":5,\"Character\":26}}," +
+                            $"\"TargetUri\":\"{new Uri(Lsp4DUtil.DefaultMainFile).AbsoluteUri}\"," +
+                            "\"TargetRange\":{\"Start\":{\"Line\":2,\"Character\":4},\"End\":{\"Line\":2,\"Character\":13}}," +
+                            "\"TargetSelectionRange\":{\"Start\":{\"Line\":2,\"Character\":4},\"End\":{\"Line\":2,\"Character\":10}}" +
+                            "}}]", JsonConvert.SerializeObject(definitions));
+        }
     }
 }
